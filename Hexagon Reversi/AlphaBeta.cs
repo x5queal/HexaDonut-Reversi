3d83a1a5f46a792a91bfc2207657e809c85c7d1f
Shachaf <shachafmeir@gmail.com>
2017-11-10 05:24:05 +0200

    Update AlphaBeta.cs

--- Hexagon Reversi/AlphaBeta.cs
+++ Hexagon Reversi/AlphaBeta.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Hexagon_Reversi
 {
+    // The static class store the BestMove & Iterate functions
     public static class AlphaBeta
     {
         const int MAXPLAYER = -1;
